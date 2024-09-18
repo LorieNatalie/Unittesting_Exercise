45b59348123279574819c2aa19cbf0688c9f1985
Lorie <wtc@pop-os.localdomain>
2024-09-18 15:05:53 +0200

    updated one

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 def EvenNumbers(n):
     num = []
     for i in n:
-     if (i % 2 == 0) :
-        num.append(i)
+      if (i % 2 == 0) :
+        num.append(str(i))
     return num
         
 def OddNumbers(n):
     apple = []
     for i in n:
-     if (i % 2 != 0 ):
-      apple.append(i)
+      if (i % 2 != 0 ):
+        apple.append(str(i))
     return apple
